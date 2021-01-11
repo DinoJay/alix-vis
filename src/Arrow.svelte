26d5556f333d6cc62948f5bc9cc6d00e4b4a87ca
Jan Maushagen <jan@wise77.vub.ac.be>
2021-01-11 17:42:19 +0100

    fixed radialLocation

---
+++ src/Arrow.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let x;
+    export let y;
+    let arrow;
+    export { arrow as this };
+</script>
+
+<div
+    bind:this={arrow}
+    class="absolute relative"
+    style="left:{x - 128}px; top: {y - 64 / 2}px; transform: translate(-0%,-0%)">
+    <div class="absolute" style="transform: translate(-56%, 0)">Alix</div>
+    <svg
+        class="w-16 h-16"
+        x="0px"
+        y="0px"
+        viewBox="0 0 31.49 31.49"
+        xml:space="preserve">
+        <path
+            style="fill:#1E201D;"
+            d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
+c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z" />
+    </svg>
+    <div class="absolute" style="transform: translate(-56%, -120%)">Dreams</div>
+</div>
--- src/Vis2.svelte
+++ src/Vis2.svelte
@@ -14,8 +14,9 @@
   import * as sel from "d3-selection";
   import Alix from "./Alix.svelte";
   import Vis from "./Vis.svelte";
-  const width = 600;
-  const height = 800;
+  import Arrow from "./Arrow.svelte";
+  const width = 375;
+  const height = 667;
   // var spiral = d3_radial
   //   .spiral([width / 2, height / 2])
   //   .increment(10)
@@ -29,12 +30,14 @@
   };
   // let translate = null;
   const placement = (nodes, cx, cy, r) => {
-    const increment = 180 / nodes.length;
-    let current = -75;
+    const increment = 180 / (nodes.length - 1);
+    console.log("increment", increment);
+    let current = -90;
+    console.log("current", 180 / (nodes.length - 1), 0);
 
     return nodes.map((d) => {
       const [tx, ty] = radialLocation([cx, cy], current, r, r, 1);
-      const [lx, ly] = radialLocation([cx, cy], current, r + 100, r + 100, 1);
+      const [lx, ly] = radialLocation([cx, cy], current, r + 50, r + 50, 1);
       current += increment;
       return { ...d, tx, ty, lx, ly };
     });
@@ -83,58 +86,50 @@
   ];
   const tmpNodes = placement(ns, width / 2, height / 2, 150);
   let nodes = [];
-  let simulation = null;
-  let cont = null;
-  onMount(() => {
-    console.log("cont", cont);
 
-    simulation = d3
-      .forceSimulation([...tmpNodes, dreams])
-      // .force("box", boxForce)
-      // .force("r", radialForce)
-      .force("collision", d3.forceCollide((d) => d.size / 2).strength(1))
-      .force("x", d3.forceX((d) => d.tx).strength(1))
-      .force("y", d3.forceY((d) => d.ty).strength(1))
-      // .force("collision", collisionForce)
-      // .force( //   "link",
-      //   d3.forceLink(links).id((d) => d.index)
-      // )
-      // .force("X", d3.forceX((d) => d.sx).strength(1))
-      // .force("Y", d3.forceY((d) => d.sy).strength(1))
-      .force("charge", d3.forceManyBody())
-      // .force("center", d3.forceCenter(width / 2, height / 2))
-      .on("tick", () => {
-        // console.log("tick", simulation.nodes());
-        nodes = simulation.nodes();
-      });
-  });
+  const simulation = d3
+    .forceSimulation([...tmpNodes, dreams])
+    // .force("box", boxForce)
+    // .force("r", radialForce)
+    .force("collision", d3.forceCollide((d) => d.size / 2).strength(1))
+    .force("x", d3.forceX((d) => d.tx).strength(1))
+    .force("y", d3.forceY((d) => d.ty).strength(1))
+    // .force("collision", collisionForce)
+    // .force( //   "link",
+    //   d3.forceLink(links).id((d) => d.index)
+    // )
+    // .force("X", d3.forceX((d) => d.sx).strength(1))
+    // .force("Y", d3.forceY((d) => d.sy).strength(1))
+    .force("charge", d3.forceManyBody())
+    // .force("center", d3.forceCenter(width / 2, height / 2))
+    .on("tick", () => {
+      // console.log("tick", simulation.nodes());
+      nodes = simulation.nodes();
+    });
 
   let bounds = null;
   let xx = 0;
   let yy = 0;
   let translate;
+
+  let arrow;
   $: {
     bounds = [
-      [
-        nodes.length ? array.min(nodes, (d) => d.lx) : 0,
-        nodes.length ? array.min(nodes, (d) => d.ly) : 0,
-      ],
-      [
-        nodes.length ? array.max(nodes, (d) => d.lx) : width,
-        nodes.length ? array.max(nodes, (d) => d.ly) : height,
-      ],
+      [array.min(nodes, (d) => d.x - d.size / 2), array.min(nodes, (d) => d.y)],
+      [array.max(nodes, (d) => d.x + d.size), array.max(nodes, (d) => d.y)],
     ];
 
     const dx = bounds[1][0] - bounds[0][0];
     const dy = bounds[1][1] - bounds[0][1];
     xx = (bounds[0][0] + bounds[1][0]) / 2;
     yy = (bounds[0][1] + bounds[1][1]) / 2;
-    console.log("bounds", bounds);
+    // console.log("bounds", bounds);
     let scale = Math.max(
       0.1,
       Math.min(20, 0.9 / Math.max(dx / (width - 50), dy / (height - 10)))
     );
 
+    // console.log("arrow", arrow);
     translate = [width / 2 - scale * xx, height / 2 - scale * yy, scale];
   }
 </script>
@@ -147,7 +142,6 @@
 
 <div
   class="relative border border-black overflow-hidden"
-  bind:this={cont}
   style="width:{width}px; height:{height}px;}">
   <div
     id="zoom-cont"
@@ -159,29 +153,25 @@
           const ns = placement(n.values, n.tx, n.ty, 200);
           const drs = placement(dreams.values.slice(0, 20), n.tx, n.ty, 300);
 
-          console.log('drs', drs);
+          // console.log('drs', drs);
           const newNodes = [n, ...ns, ...drs];
           simulation.nodes(newNodes);
           // nodes = ns;
           console.log('coll', array);
           simulation.alpha(1);
           simulation.restart();
-          console.log('bounds', bounds);
+          // console.log('bounds', bounds);
         }}
         class="absolute overflow-hidden border-2 border-black rounded-full"
         style="left:{n.x - n.size / 2}px; top:{n.y - n.size / 2}px; width:{n.size}px; height:{n.size}px; background: {n.color}; " />
     {/each}
     {#each nodes as n (n.id)}
       <div
-        class="absolute "
-        style="left:{n.lx}px; top:{n.ly}px; transform: translate(-50%, -50%)">
-        {n.id}
-      </div>
+        class="absolute bg-black w-3 h-3"
+        style="left:{n.lx}px; top:{n.ly}px; transform: translate(-50%, -50%)" />
     {/each}
+
     <!-- <div
-      class="bg-black w-3 h-3 absolute"
-      style="left:{bounds[0][0]}px; top:{bounds[0][1]}px" />
-    <div
       class="bg-black w-3 h-3 absolute"
       style="left:{bounds[1][0]}px; top:{bounds[1][1]}px" />
     <div
