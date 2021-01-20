31c38ae1bb6ea12fc5f61248ea1985497cd9098d
DinoJay <jott88@googlemail.com>
2021-01-20 16:48:24 +0100

    adv

--- src/Vis2.svelte
+++ src/Vis2.svelte
@@ -7,6 +7,8 @@
   import RadialLabels from "./RadialLabels.svelte";
   import * as sc from "d3-scale";
 
+  import { Delaunay } from "d3-delaunay";
+  import { polygonCentroid, polygonArea } from "d3-polygon";
   import * as shape from "d3-shape";
 
   import uniq from "lodash.uniqby";
@@ -38,6 +40,14 @@
     return radians * (180 / pi);
   }
 
+  const getDistance = ([x1, y1], [x2, y2]) => {
+    var a = x1 - x2;
+    var b = y1 - y2;
+
+    var c = Math.sqrt(a * a + b * b);
+    return Math.abs(c);
+  };
+
   function collide(nodeBox, otherBox) {
     const nodeLeft = nodeBox.x;
     const nodeRight = nodeBox.x + nodeBox.width;
@@ -56,7 +66,7 @@
   }
 
   const width = 775;
-  const height = 667;
+  const height = 775;
   // var spiral = d3_radial
   //   .spiral([width / 2, height / 2])
   //   .increment(10)
@@ -82,7 +92,7 @@
       .exponent(0.1)
       // .scaleSqrt()
       .domain(array.extent(nodes, (d) => d.strength))
-      .range([r + 50, 50]);
+      .range([r + 50, 20]);
 
     const maxStrength = 1 / array.max(nodes, (d) => d.strength);
     return nodes.map((d, i) => {
@@ -137,7 +147,7 @@
 
   const simulation = d3
     .forceSimulation([...nodes, dreamNode])
-    // .alphaMin(0.6)
+    .alphaMin(0.6)
     // .tick(1)
     .force(
       "collision",
@@ -160,27 +170,25 @@
       //   n.visible = true;
       // });
       // counter++;
-    })
-    .on("end", () => {
-      // textNodes.map((n, i) => {
-      //   const ns = textNodes.filter((o, j) => {
-      //     return (
-      //       nodes[i].id !== nodes[j].id && collide(n.getBBox(), o.getBBox())
-      //     );
-      //   });
-      //   console.log("ns", ns);
-      //   if (!nodes[i].taken && ns.length > 0) {
-      //     console.log("yeah", n.id);
-      //     nodes[i].visible = false;
-      //     nodes[i].taken = true;
-      //   }
-      //   // else n.visible = true;
-      // });
     });
+  // .on("end", () => {
+  //   domNodes.map((n, i) => {
+  //     const ns = domNodes.filter((o, j) => {
+  //       return collide(n.getBBox(), o.getBBox());
+  //     });
+  //     console.log("ns", ns);
+  //     if (ns.length > 0) {
+  //       console.log("yeah", n.id);
+  //       nodes[i].visible = false;
+  //     }
+  //     // else n.visible = true;
+  //   });
+  // });
 
   let bounds = null;
   let translate;
-
+  let voronoi;
+  let cells = [];
   $: {
     if (domNodes.length > 0) {
       const bbox = (i) =>
@@ -203,12 +211,17 @@
       const xx = (bounds[0][0] + bounds[1][0]) / 2;
       const yy = (bounds[0][1] + bounds[1][1]) / 2;
       let scale = Math.max(
-        0.1,
+        1,
         Math.min(20, 0.9 / Math.max(dx / width, dy / height))
       );
 
-      // console.log("nodes", nodes);
       translate = [width / 2 - scale * xx, height / 2 - scale * yy, scale];
+      const ds = nodes.map((d) => [d.x, d.y]);
+      const delaunay = Delaunay.from(ds);
+      voronoi = delaunay.voronoi([-100, -100, width + 100, height + 100]);
+
+      cells = ds.map((d, i) => [d, voronoi.cellPolygon(i)]);
+      console.log("cells", cells);
     }
   }
 
@@ -233,7 +246,7 @@
 
   let prevAngle = 0;
   const getRadius = (values) => {
-    const circum = values.length * (size + 2) * 2; //2 * Math.PI * r
+    const circum = values.length * (size + 1) * 2; //2 * Math.PI * r
     const r = Math.max(150, circum / (Math.PI * 2));
     return r;
   };
@@ -258,7 +271,7 @@
     // console.log("elems", elems);
 
     console.log("elems", elems);
-    const r = getRadius(elems) / (elems.length > 200 ? 2 : 1);
+    const r = getRadius(elems) / (elems.length > 200 ? 1.5 : 1);
     const elemNodes = placement(elems, width / 2, height / 2, r);
 
     const domain = array.extent(elemNodes, (d) =>
@@ -272,7 +285,10 @@
     });
 
     const newNodes = uniq(
-      [{ ...n, angle: 0, tx: width / 2, ty: height / 2 }, ...elemNodes],
+      [
+        { ...n, angle: 0, size: 20, tx: width / 2, ty: height / 2 },
+        ...elemNodes,
+      ],
       "id"
     );
     simulation.nodes(newNodes);
@@ -322,6 +338,23 @@
   //   simulation.alpha(1);
   //   simulation.restart();
   // };
+  const getSize = ([[x, y], cell]) => {
+    if (!cell) return 0;
+    const area = -polygonArea(cell);
+    return area;
+    // const dist = getDistance([cx, cy], [x, y]);
+    // console.log("dist", dist);
+    // return dist;
+  };
+  const getAngle = ([[x, y], cell]) => {
+    if (!cell) return 0;
+    const [cx, cy] = polygonCentroid(cell);
+    const angle = (Math.atan2(cy - y, cx - x) / Math.PI) * 2;
+    return radians_to_degrees(angle);
+    // const dist = getDistance([cx, cy], [x, y]);
+    // console.log("dist", dist);
+    // return dist;
+  };
 </script>
 
 <style>
@@ -371,32 +404,38 @@
 
     <g>
       {#each nodes as n, i (n.id)}
+        <circle
+          class="stroke-current stroke-2 text-blue-500"
+          r={n.size}
+          cx={n.x}
+          cy={n.y}
+          fill="white" />
+      {/each}
+      {#each nodes as n, i (n.id)}
         <g
-          bind:this={domNodes[i]}
           on:click={() => {
             state++;
             if (n.initial) return initialClickHandler(n);
             if (n.element) return elementClickHandler(n);
           }}>
-          <circle
-            class="stroke-current stroke-2 text-blue-500"
-            r={n.size}
-            cx={n.x}
-            cy={n.y}
-            fill="white" />
           <text
-            class={n.size < 1.4 ? 'hidden' : undefined}
-            font-size="12px"
+            bind:this={domNodes[i]}
+            style="opacity: 0.5"
+            class={cells[i] && getSize(cells[i]) < 1000 && 'hidden'}
+            font-size="10px"
             x={n.x}
             y={n.y}
             dy=".35em"
             dx={n.size + 5}
             text-anchor="start"
-            transform="rotate({(n.angle * 180) / Math.PI}, {n.x}, {n.y})">
+            transform="rotate({cells[i] && getSize(cells[i]) < 2000 ? getAngle(cells[i]) : (n.angle * 180) / Math.PI}, {n.x}, {n.y})">
             {n.title}
           </text>
+          <title>{n.title}</title>
         </g>
       {/each}
     </g>
+
+    <path fill="none" stroke="black" d={voronoi && voronoi.render()} />
   </svg>
 </div>
